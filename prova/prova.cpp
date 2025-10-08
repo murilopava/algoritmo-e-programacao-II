08c2ba2e536dca4986663c3da96f6dc7334596a3
Murilo Pavanello <160789088+murilopava@users.noreply.github.com>
2025-10-07 21:28:54 -0300

    Atualizar a resolução da prova

--- prova/prova.cpp
+++ prova/prova.cpp
@@ -3,61 +3,180 @@ using namespace std;
 
 struct Item {
     int valor;
-    Item *proximo; 
+    Item *proximo = nullptr;
 
-    void Append(int newValor){
-        if(proximo == nullptr){
+    void Append(int newValor) {
+        if (proximo == nullptr) {
             proximo = new Item;
             proximo->valor = newValor;
-        }else{
+        } else {
             proximo->Append(newValor);
         }
+    }
+
+    void InsertAt(int pos, int newValor) {
+        if (pos == 1) {
+            Item* novo = new Item;
+            novo->valor = newValor;
+            novo->proximo = proximo;
+            proximo = novo;
+        } else if (proximo != nullptr) {
+            proximo->InsertAt(pos - 1, newValor);
+        } else {
+            cout << "Posição invalida!\n";
+        }
+    }
+
+    void RemoveAt(int pos) {
+        if (proximo == nullptr) {
+            cout << "Posição invalida!\n";
+            return;
+        }
+
+        if (pos == 1) {
+            Item* temp = proximo;
+            proximo = proximo->proximo;
+            delete temp;
+        } else {
+            proximo->RemoveAt(pos - 1);
+        }
+    }
+
+    void MostrarPosicao(int pos) {
+        if (pos == 0) {
+            cout << valor << endl;
+        } else if (proximo != nullptr) {
+            proximo->MostrarPosicao(pos - 1);
+        } else {
+            cout << "Posição invalida!\n";
+        }
+    }
+
+    void MostrarNormal() {
+        cout << valor << " ";
+        if (proximo != nullptr)
+            proximo->MostrarNormal();
+    }
+
+    void MostrarInversa() {
+        if (proximo != nullptr)
+            proximo->MostrarInversa();
+        cout << valor << " ";
+    }
+
+    void Liberar() {
+        if (proximo != nullptr)
+            proximo->Liberar();
+        delete this;
     }
 };
 
 struct ListaInteiro {
-
-    Item *primeiroItem;
+    Item *primeiroItem = nullptr;
     int qt = 0;
 
-    int addFinalDaLista(int valor) {
-        if(primeiroItem == nullptr){
+    void addFinalDaLista(int valor) {
+        if (primeiroItem == nullptr) {
             primeiroItem = new Item;
             primeiroItem->valor = valor;
-        }else{
+        } else {
             primeiroItem->Append(valor);
         }
+        qt++;
     }
 
-    int addPosicao(int posicao, int valor) {
-        if(qt == posicao) {
-            primeiroItem = new Item;
-            primeiroItem->valor = valor;
-        } else{
-            qt += 1;
+    void addPosicao(int pos, int valor) {
+        if (pos < 0 || pos > qt) {
+            cout << "Posição invalida!\n";
+            return;
         }
+
+        if (pos == 0) {
+            Item *novo = new Item;
+            novo->valor = valor;
+            novo->proximo = primeiroItem;
+            primeiroItem = novo;
+        } else {
+            primeiroItem->InsertAt(pos - 1, valor);
+        }
+        qt++;
     }
 
-    int removerItem(int posicao, int item) {
+    void removerItem(int pos) {
+        if (primeiroItem == nullptr || pos < 0 || pos >= qt) {
+            cout << "Posição invalida!\n";
+            return;
+        }
 
+        if (pos == 0) {
+            Item *temp = primeiroItem;
+            primeiroItem = primeiroItem->proximo;
+            delete temp;
+        } else {
+            primeiroItem->RemoveAt(pos - 1);
+        }
+        qt--;
     }
 
-    int exibirItem(int posicao) {
-
+    void exibirItem(int pos) {
+        if (primeiroItem == nullptr) {
+            cout << "Lista vazia!\n";
+            return;
+        }
+        primeiroItem->MostrarPosicao(pos);
     }
 
     void exibirLista() {
-
+        if (primeiroItem == nullptr) {
+            cout << "Lista vazia!\n";
+            return;
+        }
+        primeiroItem->MostrarNormal();
+        cout << endl;
     }
 
     void exibirListaInversa() {
+        if (primeiroItem == nullptr) {
+            cout << "Lista vazia!\n";
+            return;
+        }
+        primeiroItem->MostrarInversa();
+        cout << endl;
+    }
 
+    void liberarMemoria() {
+        if (primeiroItem != nullptr) {
+            primeiroItem->Liberar();
+            primeiroItem = nullptr;
+        }
+        qt = 0;
     }
 };
 
-
-int main(){
+int main() {
     ListaInteiro lista;
 
-    lista.addFinalDaLista(4);
+
+    lista.addFinalDaLista(5);
+    lista.addFinalDaLista(8);
+    lista.addFinalDaLista(12);
+
+    cout << "Lista normal: ";
+    lista.exibirLista();
+
+    lista.addPosicao(1, 25);
+    cout << "Inserindo em uma posicao: ";
+    lista.exibirLista();
+
+    lista.removerItem(2);
+    cout << "Removendo em uma posicao: ";
+    lista.exibirLista();
+
+    cout << "Exibindo valor especifico na lista: ";
+    lista.exibirItem(1);
+
+    cout << "Lista inversa: ";
+    lista.exibirListaInversa();
+
+    lista.liberarMemoria();
 }
